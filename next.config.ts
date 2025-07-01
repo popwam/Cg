da4c736741736fce3509375c6def2700151afa03
popwam <mmdohsaad39@gmail.com>
2025-07-01 15:44:38 +0300

    fix: disable eslint errors on build

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // تعطيل أخطاء ESLint وقت الـ build
-  },
-};
-
-export default nextConfig;
