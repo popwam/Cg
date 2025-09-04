fd8b805dc76162b7f9edaa5d0d5f2cb5bbdbfae4
POP WAM <124102501+popwam@users.noreply.github.com>
2025-09-05 02:04:26 +0300

    Update metadata.ts

--- src/app/ar/about/metadata.ts
+++ src/app/ar/about/metadata.ts
@@ -20,6 +20,7 @@ export const metadata = {
     "مشروعات مصر العقارية",
     "عقارات فاخرة",
   ],
+  viewport: 'width=device-width, initial-scale=1.0',
   openGraph: {
     title: "من نحن - كابيتال جيت",
     description: "تعرف على رؤية ورسالة كابيتال جيت العقارية.",
@@ -36,7 +37,6 @@ export const metadata = {
     card: "summary_large_image",
     title: "من نحن - كابيتال جيت",
     description: "تعرف على رؤية ورسالة كابيتال جيت العقارية.",
-    viewport: 'width=device-width, initial-scale=1.0',
     images: [
       "https://res.cloudinary.com/dfltsy2ov/image/upload/v1753623051/logo/sxdinb9u5qqkth3ewals.avif",
     ],
