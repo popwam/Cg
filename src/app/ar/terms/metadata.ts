701bcaac772806386260bcee99723de793ce075f
POP WAM <124102501+popwam@users.noreply.github.com>
2025-09-05 01:39:43 +0300

    Update metadata.ts

--- src/app/ar/terms/metadata.ts
+++ src/app/ar/terms/metadata.ts
@@ -38,6 +38,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "الشروط والأحكام - كابيتال جيت",
     description: "تعرف على شروط استخدامك لمنصة كابيتال جيت.",
+    viewport: 'width=device-width, initial-scale=1.0',
     images: [
       "https://res.cloudinary.com/dfltsy2ov/image/upload/v1753623051/logo/sxdinb9u5qqkth3ewals.avif",
     ],
