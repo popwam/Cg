fed650014833c509ca5e714563dbbd02c0133e01
POP WAM <124102501+popwam@users.noreply.github.com>
2025-09-05 01:36:33 +0300

    Update metadata.ts

--- src/app/ar/privacy/metadata.ts
+++ src/app/ar/privacy/metadata.ts
@@ -36,6 +36,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "سياسة الخصوصية - كابيتال جيت",
     description: "اطلع على كيفية تعاملنا مع بياناتك ومعلوماتك.",
+    viewport: 'width=device-width, initial-scale=1.0',
     images: [
       "https://res.cloudinary.com/dfltsy2ov/image/upload/v1753623051/logo/sxdinb9u5qqkth3ewals.avif",
     ],
