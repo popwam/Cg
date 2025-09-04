e917e4ae4e9391ae47d642a745c113ee6ec2576f
POP WAM <124102501+popwam@users.noreply.github.com>
2025-09-05 01:26:30 +0300

    Update metadata.ts

--- src/app/privacy/metadata.ts
+++ src/app/privacy/metadata.ts
@@ -40,6 +40,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "Privacy Policy - Capital Gate",
     description: "Learn how we handle your data and privacy.",
+    viewport: 'width=device-width, initial-scale=1.0',
     images: [
       "https://res.cloudinary.com/dfltsy2ov/image/upload/v1753623051/logo/sxdinb9u5qqkth3ewals.avif",
     ],
