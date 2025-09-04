c660300ba7221246b3761372ce165589800a7969
POP WAM <124102501+popwam@users.noreply.github.com>
2025-09-05 02:04:05 +0300

    Update metadata.ts

--- src/app/ar/contact/metadata.ts
+++ src/app/ar/contact/metadata.ts
@@ -19,6 +19,7 @@ export const metadata = {
     "مشروعات مصر العقارية",
     "عقارات فاخرة",
   ],
+  viewport: 'width=device-width, initial-scale=1.0',
   openGraph: {
     title: "تواصل معنا - كابيتال جيت",
     description: "تواصل معنا لأي استفسار بخصوص خدماتنا العقارية.",
@@ -35,7 +36,6 @@ export const metadata = {
     card: "summary_large_image",
     title: "تواصل معنا - كابيتال جيت",
     description: "تواصل معنا لأي استفسار بخصوص خدماتنا العقارية.",
-    viewport: 'width=device-width, initial-scale=1.0',
     images: [
       "https://res.cloudinary.com/dfltsy2ov/image/upload/v1753623051/logo/sxdinb9u5qqkth3ewals.avif",
     ],
