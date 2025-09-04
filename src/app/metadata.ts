a11954850e0c50e43d009a4b9902904424b4949e
POP WAM <124102501+popwam@users.noreply.github.com>
2025-09-05 01:22:47 +0300

    Update metadata.ts

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -43,6 +43,7 @@ export const metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Home - Capital Gate",
+    viewport: 'width=device-width, initial-scale=1.0',
     description: "Discover the best real estate projects with Capital Gate.",
     images: [
       "https://res.cloudinary.com/dfltsy2ov/image/upload/v1753623051/logo/sxdinb9u5qqkth3ewals.avif",
