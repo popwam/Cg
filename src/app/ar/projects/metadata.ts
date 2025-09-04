66ca0b98f15988d2610147a1b82b3fe391750763
POP WAM <124102501+popwam@users.noreply.github.com>
2025-09-05 01:39:02 +0300

    Update metadata.ts

--- src/app/ar/projects/metadata.ts
+++ src/app/ar/projects/metadata.ts
@@ -32,6 +32,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "المشروعات - كابيتال جيت",
     description: "استكشف أحدث المشروعات العقارية وأكثرها شهرة.",
+    viewport: 'width=device-width, initial-scale=1.0',
     images: [
       "https://res.cloudinary.com/dfltsy2ov/image/upload/v1753623051/logo/sxdinb9u5qqkth3ewals.avif",
     ],
